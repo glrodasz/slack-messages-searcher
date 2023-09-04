18742b4954d5cc8d6a93ed47d11363d91ca3557b
Guillermo Rodas <guillermo.rodas@eqtpartners.com>
2023-09-04 14:47:32 +0200

    Allow JSON as well

--- README.md
+++ README.md
@@ -4,8 +4,20 @@ This project uses `pyenv` to manage dependencies.
 
 1. Install it with `pip install pyenv`
 2. Run `pyenv install` to install dependencies.
+3. Copy `.env.example` into `.env` and set the slack token.
 
 ## How to use
 
-1. Copy `.env.example` into `.env` and set the slack token.
-2. Run the script with: `python search.py "your_query_message_here"`
+Run the script:
+
+For JSON (default):
+
+```
+python script_name.py "your_query_message_here"
+```
+
+For CSV:
+
+```
+python script_name.py "your_query_message_here" csv
+```
--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 import csv
 from slack_bolt import App
 from dotenv import load_dotenv
@@ -13,44 +14,52 @@ SLACK_TOKEN = os.getenv("SLACK_TOKEN")
 # Initialize the app with your bot token
 app = App(token=SLACK_TOKEN)
 
+def search_messages(query, format="json"):
+    all_messages = []
+    
+    next_cursor = None
+    
+    while True:
+        params = {
+            "query": query,
+            "count": 100  # Fetch 100 messages at a time; adjust as needed
+        }
+        
+        if next_cursor:
+            params["cursor"] = next_cursor
+            
+        # Use the search_messages method
+        response = app.client.search_messages(**params)
+        
+        # Append the results to the all_messages list
+        all_messages.extend(response['messages']['matches'])
 
-def search_messages(query):
-    # Open a CSV file for writing
-    with open("messages.csv", "w", newline='') as csvfile:
-        fieldnames = ['username', 'text']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
+        next_cursor = response.get("response_metadata", {}).get("next_cursor")
+        if not next_cursor:
+            break
 
-        next_cursor = None
-
-        while True:
-            params = {
-                "query": query,
-                "count": 100  # Fetch 100 messages at a time; adjust as needed
-            }
-
-            if next_cursor:
-                params["cursor"] = next_cursor
-
-            # Use the search_messages method
-            response = app.client.search_messages(**params)
-
-            # Write the results to the CSV file
-            for message in response['messages']['matches']:
+    # Save the results based on the specified format
+    if format == "json":
+        with open("messages.json", "w") as json_file:
+            json.dump(all_messages, json_file, indent=4)
+    elif format == "csv":
+        with open("messages.csv", "w", newline='') as csvfile:
+            fieldnames = ['username', 'text']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for message in all_messages:
                 writer.writerow({
                     'username': message['username'],
                     'text': message['text']
                 })
 
-            next_cursor = response.get(
-                "response_metadata", {}).get("next_cursor")
-            if not next_cursor:
-                break
-
-
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         query = sys.argv[1]
-        search_messages(query)
+        format = sys.argv[2] if len(sys.argv) > 2 else "json"
+        if format not in ["json", "csv"]:
+            print("Invalid format. Please choose either 'json' or 'csv'.")
+            sys.exit(1)
+        search_messages(query, format)
     else:
         print("Please provide a query message as an argument.")
