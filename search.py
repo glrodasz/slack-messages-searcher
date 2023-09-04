19ac818ae1c01c6b789b8ea3cc6a48621bf6cfee
Guillermo Rodas <guillermo.rodas@eqtpartners.com>
2023-09-04 14:26:07 +0200

    Search all messages

--- search.py
+++ search.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import csv
 from slack_bolt import App
 from dotenv import load_dotenv
 
@@ -11,16 +13,40 @@ SLACK_TOKEN = os.getenv("SLACK_TOKEN")
 # Initialize the app with your bot token
 app = App(token=SLACK_TOKEN)
 
+
 def search_messages(query):
-    # Your hardcoded search query
-    query = "{query}"
-    
-    # Use the search_messages method
-    response = app.client.search_messages(query=query)
-    
-    # Print the results
-    for message in response['messages']['matches']:
-        print(f"User: {message['username']}, Text: {message['text']}")
+    # Open a CSV file for writing
+    with open("messages.csv", "w", newline='') as csvfile:
+        fieldnames = ['username', 'text']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        next_cursor = None
+
+        while True:
+            params = {
+                "query": query,
+                "count": 100  # Fetch 100 messages at a time; adjust as needed
+            }
+
+            if next_cursor:
+                params["cursor"] = next_cursor
+
+            # Use the search_messages method
+            response = app.client.search_messages(**params)
+
+            # Write the results to the CSV file
+            for message in response['messages']['matches']:
+                writer.writerow({
+                    'username': message['username'],
+                    'text': message['text']
+                })
+
+            next_cursor = response.get(
+                "response_metadata", {}).get("next_cursor")
+            if not next_cursor:
+                break
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
